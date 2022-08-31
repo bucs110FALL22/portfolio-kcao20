e81c9773ae91508a563fb6f106cfa6dfe0cc8a9b
kcao20 <kcao2022@gmail.com>
2022-08-31 12:38:21 -0400

    finish ch01 exercise

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,18 @@
+# part 1
+print(10 * 5)
+print(10**2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)  # not exact, trailing 6's
+
+# part 2
+rate = float(input("What is the current exchange rate from Euros to Dollars? "))
+amount = float(input("How much currency do you want to exchange? "))
+total = amount * rate
+result = total - 3
+print(f"{result:.2f}")

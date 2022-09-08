56dbac1f70225158232e438f7977fee0ded883c1
kcao20 <kcao2022@gmail.com>
2022-09-08 11:46:27 -0400

    print out type for every var

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,9 +1,14 @@
 import random
-#Part A
+
+# Part A
 weeks = 16
+print(weeks, type(weeks))
 classes = 5
+print(classes, type(classes))
 tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
+print(tuition, type(tuition))
+cost_per_week = (tuition / classes) / weeks
+print(cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
 
 classes_per_week = 3
@@ -13,7 +18,18 @@ print(cost_per_class, type(cost_per_class))
 print("Your cost per class amount to:", cost_per_class)
 
 
-#Part B
-keyboardSwitches = ["alpaca", "cream", "tangerine", "boba", "zealio", "oil king", "silk", "cherry", "gateron", "kalih"]
+# Part B
+keyboardSwitches = [
+    "alpaca",
+    "cream",
+    "tangerine",
+    "boba",
+    "zealio",
+    "oil king",
+    "silk",
+    "cherry",
+    "gateron",
+    "kalih",
+]
 switchChoice = random.choice(keyboardSwitches)
 print(f"You should build your next keyboard using {switchChoice} switches.")

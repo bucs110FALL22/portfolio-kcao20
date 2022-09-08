6e659f9a00a7b4d72064e8abd85127846e55bce8
kcao20 <kcao2022@gmail.com>
2022-09-08 11:33:06 -0400

    Ch 2 Lab - Simple Data

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,4 +1,4 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
@@ -6,5 +6,14 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
+cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class))
+print("Your cost per class amount to:", cost_per_class)
+
 
 #Part B
+keyboardSwitches = ["alpaca", "cream", "tangerine", "boba", "zealio", "oil king", "silk", "cherry", "gateron", "kalih"]
+switchChoice = random.choice(keyboardSwitches)
+print(f"You should build your next keyboard using {switchChoice} switches.")

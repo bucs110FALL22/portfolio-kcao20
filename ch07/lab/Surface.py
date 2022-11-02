4ed289158eed92eddafe96749bde2c8305bb2798
kcao20 <kcao2022@gmail.com>
2022-11-01 20:58:45 -0400

    ch07 lab

---
+++ ch07/lab/Surface.py
@@ -0,0 +1,10 @@
+import Rectangle
+
+
+class Surface:
+    def __init__(self, filename, x, y, h, w):
+        self.image = filename
+        self.rect = Rectangle.Rectangle(x, y, h, w)
+
+    def getRect(self):
+        return self.rect

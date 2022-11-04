d84ca87bc582cd5ef929c5082ff3d9f0c7afe951
kcao20 <kcao2022@gmail.com>
2022-11-04 13:06:38 -0400

    add docstrings to ch07 lab

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,9 +1,18 @@
 class Rectangle:
-    def __init__(self, x, y, h, w):
+    def __init__(self, x=0, y=0, h=0, w=0):
+        """
+        Initializes Rectangle object
+        args: x, y, height, width
+        """
         self.x = max(0, x)
         self.y = max(0, y)
         self.height = max(0, h)
         self.width = max(0, w)
 
     def __str__(self):
-        return f"(x:{self.x}, y:{self.y} width:{self.width}, height:{self.height})"
+        """
+        Replaces the default str method and returns all variables values of the object
+        args: self
+        return: str
+        """
+        return f"(x:{self.x}, y:{self.y}, height:{self.height}, width:{self.width})"
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,10 +1,18 @@
-import Rectangle
+from Rectangle import Rectangle
 
 
 class Surface:
-    def __init__(self, filename, x, y, h, w):
+    def __init__(self, filename, x=0, y=0, h=0, w=0):
+        """
+        Initializes Surface Object
+        args: filename, x, y, height, width
+        """
         self.image = filename
-        self.rect = Rectangle.Rectangle(x, y, h, w)
+        self.rect = Rectangle(x, y, h, w)
 
     def getRect(self):
+        """
+        Returns the rectangle of the surface
+        return: Rectangle
+        """
         return self.rect
